44495299bdbbd49ac7acffa4c624fa0520485667
Syed Abdul Aziz <syedabdulaziz531@gmail.com>
2025-11-30 13:12:27 +0500

    Fix db issues

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,8 +3,11 @@ import { authConfig } from "@/app/auth/config";
 
 export const runtime = "nodejs";
 
+const AUTH_SECRET = process.env.AUTH_SECRET || "rkibxM3dsw2LYlK5zIPUY4Tn2ili1P7dgaRdBVe/eU4=";
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
   ...authConfig,
+  secret: AUTH_SECRET,
   trustHost: true,
 });
 
